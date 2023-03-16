10c851bfa64e9be33d18bbdf852bc2d42f1ea6a5
Iago Henrique <theiago@live.com>
2023-03-15 21:10:59 -0300

    adicionado verificação nas notas

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ def main():
     nota1 = request.args.get("nota1", type=float)
     nota2 = request.args.get("nota2", type=float)
     if nota1 and nota2:
-        if nota1 > 10 or nota2 > 10:
-            return "Insira notas válidas"
+        if nota1 > 10 or nota2 > 10 or nota1 < 0 or nota2 < 0:
+            return "<h1>Insira notas válidas</h1>"
         media = str((nota1 + nota2) / 2)
         return f"<h1>A sua média é de {media}</h1>"
+    return "<h1>Insira ambas notas.</h1>"
 
 if __name__ == '__main__':
     app.run()
